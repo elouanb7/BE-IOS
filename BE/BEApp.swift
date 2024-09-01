3e5198afdb57fe05ae8535e83e09bf4ee3e2cefd
elouanb7 <elouanb7@gmail.com>
2024-09-01 16:48:13 +0200

    Initial Commit

---
+++ BE/BEApp.swift
@@ -0,0 +1,17 @@
+//
+//  BEApp.swift
+//  BE
+//
+//  Created by Elouan Bessettes on 01/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
